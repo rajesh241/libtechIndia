06caa3950ade87d1b2396661b4f6829db7bc8c85
rajesh241 <togoli@gmail.com>
2019-09-10 04:58:59 +0530

    Updated requirements.txt

--- web/src/config/defines.py
+++
@@ -1,6 +0,0 @@
-LIBTECH_AWS_ACCESS_KEY_ID = "AKIA35VGRZJ26QJ3Z7H7"
-LIBTECH_AWS_SECRET_ACCESS_KEY = "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"
-LIBTECH_AWS_BUCKET_NAME="libtech-india-django"
-AWS_S3_REGION_NAME = 'ap-south-1'
-djangoDir="/home/crawler/repo/libtechIndia/web/src/"
-djangoSettings="libtech.settings"
