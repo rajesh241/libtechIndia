752a42f78e4da3f889ce1d77b8579b1bad5a8bb8
Rajesh Golani <rajeshgolani@gmail.com>
2020-08-22 16:09:04 +0530

    Deleted old code

--- backend/src_bk/base/custom_settings.py
+++
@@ -1,4 +0,0 @@
-"""This module has custom settings for Libtech Django backend"""
-from pathlib import Path
-HOMEDIR = str(Path.home())
-SQL_CONFIG = f"{HOMEDIR}/.libtech/libtech_backend_mysql.cnf"
--- backend/src_bk/custom_settings.py
+++
@@ -1,2 +0,0 @@
-"""This module has custom settings for Libtech Django backend"""
-from pathlib import Path
